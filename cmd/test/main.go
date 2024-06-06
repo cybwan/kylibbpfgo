cfa270b04fbb7dd5b78ebe126501a5dba55d6d61
Cybwan <baili@flomesh.io>
2024-06-06 14:06:56 +0800

    init

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -14,9 +14,11 @@ func main() {
 	test_fd, _ := kylibbpfgo.OpenObjPinned("/sys/fs/bpf/f4gw_progs")
 	fmt.Println("test_fd:", test_fd)
 
-	k := uint32(1)
-	v := uint32(14)
-	if err := kylibbpfgo.Update(test_fd, unsafe.Pointer(&k), unsafe.Pointer(&v)); err != nil {
+	egress_fd, _ := kylibbpfgo.OpenObjPinned("/sys/fs/bpf/gateway/xdp_egress")
+	fmt.Println("ingress_fd:", egress_fd)
+
+	k := uint32(2)
+	if err := kylibbpfgo.Update(test_fd, unsafe.Pointer(&k), unsafe.Pointer(&egress_fd)); err != nil {
 		fmt.Println(err.Error())
 	}
 }
