6903fd96cce2e2ffcf9e0433a52b450723e26283
Cybwan <baili@flomesh.io>
2024-06-06 13:40:04 +0800

    init

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -11,12 +11,12 @@ func main() {
 	numPossibleCPUs, _ := kylibbpfgo.NumPossibleCPUs()
 	fmt.Println("numPossibleCPUs:", numPossibleCPUs)
 
-	f4gw_progs_fd, _ := kylibbpfgo.OpenObjPinned("/sys/fs/bpf/f4gw_progs")
-	fmt.Println("f4gw_progs_fd:", f4gw_progs_fd)
+	test_fd, _ := kylibbpfgo.OpenObjPinned("/sys/fs/bpf/test")
+	fmt.Println("test_fd:", test_fd)
 
 	k := uint32(0)
 	v := uint32(5)
-	if err := kylibbpfgo.Update(f4gw_progs_fd, unsafe.Pointer(&k), unsafe.Pointer(&v)); err != nil {
+	if err := kylibbpfgo.Update(test_fd, unsafe.Pointer(&k), unsafe.Pointer(&v)); err != nil {
 		fmt.Println(err.Error())
 	}
 }
