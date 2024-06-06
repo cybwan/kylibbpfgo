74127d9d10f95f69d28392fd6c9f1031e0ca762c
Cybwan <baili@flomesh.io>
2024-06-06 10:53:39 +0800

    init

--- map-common.go
+++ map-common.go
@@ -102,22 +102,6 @@ const (
 	MapFlagFLock                        // spin_lock-ed map_lookup/map_update
 )
 
-//
-// BPFMapInfo
-//
-
-// BPFMapInfo mirrors the C structure bpf_map_info.
-type BPFMapInfo struct {
-	Type       MapType
-	ID         uint32
-	KeySize    uint32
-	ValueSize  uint32
-	MaxEntries uint32
-	MapFlags   uint32
-	Name       string
-	MapExtra   uint64
-}
-
 // GetMapFDByID returns a file descriptor for the map with the given ID.
 func GetMapFDByID(id uint32) (int, error) {
 	fdC := C.bpf_map_get_fd_by_id(C.uint(id))
@@ -126,29 +110,6 @@ func GetMapFDByID(id uint32) (int, error) {
 	}
 
 	return int(fdC), nil
-}
-
-// GetMapInfoByFD returns the BPFMapInfo for the map with the given file descriptor.
-func GetMapInfoByFD(fd int) (*BPFMapInfo, error) {
-	infoC := C.cgo_bpf_map_info_new()
-	defer C.cgo_bpf_map_info_free(infoC)
-
-	infoLenC := C.cgo_bpf_map_info_size()
-	retC := C.bpf_map_get_info_by_fd(C.int(fd), infoC, &infoLenC)
-	if retC < 0 {
-		return nil, fmt.Errorf("failed to get map info for fd %d: %w", fd, syscall.Errno(-retC))
-	}
-
-	return &BPFMapInfo{
-		Type:       MapType(C.cgo_bpf_map_info_type(infoC)),
-		ID:         uint32(C.cgo_bpf_map_info_id(infoC)),
-		KeySize:    uint32(C.cgo_bpf_map_info_key_size(infoC)),
-		ValueSize:  uint32(C.cgo_bpf_map_info_value_size(infoC)),
-		MaxEntries: uint32(C.cgo_bpf_map_info_max_entries(infoC)),
-		MapFlags:   uint32(C.cgo_bpf_map_info_map_flags(infoC)),
-		Name:       C.GoString(C.cgo_bpf_map_info_name(infoC)),
-		MapExtra:   uint64(C.cgo_bpf_map_info_map_extra(infoC)),
-	}, nil
 }
 
 // CalcMapValueSize calculates the size of the value for a map.
