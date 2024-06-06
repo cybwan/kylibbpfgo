60fb96bdc000ec86e1aa85ba61aba3de3ae1922e
Cybwan <baili@flomesh.io>
2024-06-06 13:15:01 +0800

    init

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 
 	"github.com/cybwan/kylibbpfgo"
 )
@@ -12,4 +13,10 @@ func main() {
 
 	f4gw_progs_fd, _ := kylibbpfgo.OpenObjPinned("/sys/fs/bpf/f4gw_progs")
 	fmt.Println("f4gw_progs_fd:", f4gw_progs_fd)
+
+	k := uint32(0)
+	v := uint32(5)
+	if err := kylibbpfgo.Update(f4gw_progs_fd, unsafe.Pointer(&k), unsafe.Pointer(&v)); err != nil {
+		fmt.Println(err.Error())
+	}
 }
--- map-low.go
+++ map-low.go
@@ -11,39 +11,41 @@ import (
 	"unsafe"
 )
 
+type MapFlag uint32
+
+const (
+	MapFlagUpdateAny     MapFlag = iota // create new element or update existing
+	MapFlagUpdateNoExist                // create new element if it didn't exist
+	MapFlagUpdateExist                  // update existing element
+	MapFlagFLock                        // spin_lock-ed map_lookup/map_update
+)
+
 //
-// BPFMapLow (low-level API)
+// bpf map (low-level API)
 //
 
-// BPFMapLow provides a low-level interface to BPF maps.
-// Its methods follow the BPFMap naming convention.
-type BPFMapLow struct {
-	fd int
+func Update(fd int, key, value unsafe.Pointer) error {
+	return UpdateValueFlags(fd, key, value, MapFlagUpdateAny)
 }
 
-func (m *BPFMapLow) Update(key, value unsafe.Pointer) error {
-	return m.UpdateValueFlags(key, value, MapFlagUpdateAny)
-}
-
-func (m *BPFMapLow) UpdateValueFlags(key, value unsafe.Pointer, flags MapFlag) error {
+func UpdateValueFlags(fd int, key, value unsafe.Pointer, flags MapFlag) error {
 	retC := C.bpf_map_update_elem(
-		C.int(m.fd),
+		C.int(fd),
 		key,
 		value,
 		C.ulonglong(flags),
 	)
 	if retC < 0 {
-		return fmt.Errorf("failed to update map %d: %w", m.fd, syscall.Errno(-retC))
+		return fmt.Errorf("failed to update map %d: %w", fd, syscall.Errno(-retC))
 	}
 
 	return nil
 }
 
-func (m *BPFMapLow) DeleteKey(key unsafe.Pointer) error {
-	retC := C.bpf_map_delete_elem(C.int(m.fd), key)
+func DeleteKey(fd int, key unsafe.Pointer) error {
+	retC := C.bpf_map_delete_elem(C.int(fd), key)
 	if retC < 0 {
-		return fmt.Errorf("failed to delete key %d in map %d: %w", key, m.fd, syscall.Errno(-retC))
+		return fmt.Errorf("failed to delete key %d in map %d: %w", key, fd, syscall.Errno(-retC))
 	}
-
 	return nil
 }
